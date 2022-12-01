3d91cccab44f2dcd46e42bacaed49f929564cfc8
hodisan94 <81373224+hodisan94@users.noreply.github.com>
2022-12-01 10:45:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import './App.css';
 import React from "react";
-
-import mainStuff from "./mainStuff";
-
 import {useState,useEffect,useRef} from 'react';
 
 
-const App = ()=> {
-  
-  const [isActive, setIsActive] = useState(false);
-  const [showPlot, setShowPlot] = useState();
+import mainStuff from "./mainStuff";
+import './App.css'
+
+
+
+const App = () => {
+
+
+
+  const ref = useRef(null);
+
+  const [isActive , setIsActive] = useState(false);
+  const [showPlot, setShowPlot] =useState();
 
   const showFullPlot = async (plot_url) => {
     setShowPlot(plot_url)
+    console.log(plot_url)
     setIsActive(true);
   }
+
+
+
+
+
   return (
-    <div className="App">
-      <header>
+    <div className="app">
+            <header>
         <div class="topnav">
         <a class="active" href="#home">Home</a>
         <a href="#news">News</a>
@@ -26,56 +37,100 @@ const App = ()=> {
         </div>
       </header>
       <br></br>
+      {/* <h1>Birds For The Win</h1> */}
       <div className="mainApp">
-        <table><tr>
-        <div className='scroll-left'>
-          <div className='plot'>
-            <ul>
-            <img src='https://labwrite.ncsu.edu/res/gh/2d-scatter-linreg.gif'
-            onClick={() => showFullPlot('https://labwrite.ncsu.edu/res/gh/2d-scatter-linreg.gif')}/>
-            </ul>
-            <ul>
-            <img src='https://www.data-to-viz.com/graph/scatter_files/figure-html/unnamed-chunk-1-1.png'
-            onClick={() => showFullPlot('https://www.data-to-viz.com/graph/scatter_files/figure-html/unnamed-chunk-1-1.png')}/>
-            </ul>
-            <ul>
-            <img src='https://www.mathworks.com/help/examples/matlab/win64/LoadAndPlotDataFromTextFileExample_01.png'
-            onClick={() => showFullPlot('https://www.mathworks.com/help/examples/matlab/win64/LoadAndPlotDataFromTextFileExample_01.png')}/>
-            </ul>
-            <ul>
-              <img src='https://opensource.com/sites/default/files/uploads/seaborn.png'
-              onClick={() => showFullPlot('https://opensource.com/sites/default/files/uploads/seaborn.png')}/>
-            </ul>
-            <ul>
-              <img
-              src='https://d2mvzyuse3lwjc.cloudfront.net/doc/en/Tutorial/images/Transparent_Parametric_Surfaces_combined_with_Scatter_Plot/Transparent_Parametric_Surfaces_06.png?v=47973'
-              onClick={() => showFullPlot('https://d2mvzyuse3lwjc.cloudfront.net/doc/en/Tutorial/images/Transparent_Parametric_Surfaces_combined_with_Scatter_Plot/Transparent_Parametric_Surfaces_06.png?v=47973')}/>
-            </ul>
+      {/* <div className="bird">
+
+      </div> */}
+
+      <div className="scroll-left"  ref={ref}>
+        <span>
+          <ul>
+        
+            <div className="container">
+           
+                  <div className="bird">
+                  <div>
+                  <ul>
+                    <img src='https://labwrite.ncsu.edu/res/gh/2d-scatter-linreg.gif'
+                    onClick={() => showFullPlot('https://labwrite.ncsu.edu/res/gh/2d-scatter-linreg.gif')}/>
+                    </ul>
+                    <br></br>
+                    <ul>
+                    <img src='https://www.data-to-viz.com/graph/scatter_files/figure-html/unnamed-chunk-1-1.png'
+                    onClick={() => showFullPlot('https://www.data-to-viz.com/graph/scatter_files/figure-html/unnamed-chunk-1-1.png')}/>
+                    </ul>
+                    <br></br>
+
+                    <ul>
+                    <img src='https://www.mathworks.com/help/examples/matlab/win64/LoadAndPlotDataFromTextFileExample_01.png'
+                    onClick={() => showFullPlot('https://www.mathworks.com/help/examples/matlab/win64/LoadAndPlotDataFromTextFileExample_01.png')}/>
+                    </ul>
+                    <br></br>
+
+                    <ul>
+                      <img src='https://opensource.com/sites/default/files/uploads/seaborn.png'
+                      onClick={() => showFullPlot('https://opensource.com/sites/default/files/uploads/seaborn.png')}/>
+                    </ul>
+                    <br></br>
+
+                    <ul>
+                      <img
+                      src='https://d2mvzyuse3lwjc.cloudfront.net/doc/en/Tutorial/images/Transparent_Parametric_Surfaces_combined_with_Scatter_Plot/Transparent_Parametric_Surfaces_06.png?v=47973'
+                      onClick={() => showFullPlot('https://d2mvzyuse3lwjc.cloudfront.net/doc/en/Tutorial/images/Transparent_Parametric_Surfaces_combined_with_Scatter_Plot/Transparent_Parametric_Surfaces_06.png?v=47973')}/>
+                    </ul>
+                  </div>
+
+                  <br/>
+              </div>
           </div>
 
-          
-        </div>
-        </tr>
-        <tr>
-        <div className='center'>
+        
+          </ul>
+        </span>
+      </div>
+
+      <div className="center" >
+        {
+      isActive
+        ? (
+
+        <table>
+          <tr>
           <td>
-          <img src='https://earth3dmap.com/wp-content/uploads/2020/01/political-map-of-the-world.png'/>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/c/ca/Israel_outline_north_negev.png'/>
+          <div>
+            <button>
+              <b>Upload new data</b>
+            </button>
+          </div>
 
 
           </td>
           <td>
           <img src={showPlot}/>
+          <div>
+            <button>
+              <b>Download Data</b>
+            </button>
+          </div>
 
           </td>
-
-          </div>
           </tr>
-          </table>
-      </div>
-      
+        </table>
+        ):
+        (
+          <div className="empty">
+            <h2>Nothing to show</h2>
+          </div>
+        )
+}
 
+      </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
